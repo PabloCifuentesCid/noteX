8935c8cee9ea6a94c46e8fe2e006d4ce509496c0
PabloCifuentesCid <holapablocifuentes@gmail.com>
2021-10-05 13:56:23 +0000

    Edit and destroy

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -21,10 +21,17 @@ class DocsController < ApplicationController
         end 
     end
     
-    def update  
+    def update
+        if @doc.update(doc_params)
+            redirect_to @doc   
+        else
+            render 'edit'
+        end  
     end
     
     def destroy
+        @doc.destroy
+        redirect_to docs_path
     end
     
     private
