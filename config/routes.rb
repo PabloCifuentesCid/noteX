b4e4a8ca951a74098054fd882b652941b002f7d2
PabloCifuentesCid <holapablocifuentes@gmail.com>
2021-10-18 14:38:40 +0000

    index page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
-    
+  devise_for :users  
   resources :docs
   authenticated :user do
-    root 'docs#index', as 'authenticated_root'
+    root 'docs#index', as: 'authenticated_root'
   end
+  get 'welcome/index'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
